975b356dc0ab15a7cbe8a7903bc51b0bea902f51
Justin Yian Liao <jliao@roblox.com>
2026-04-15 15:10:20 -0700

    Fix leader-/ search lazy updating

--- nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/lua/kickstart/plugins/telescope.lua
@@ -98,10 +98,10 @@ return {
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
+          sorting_strategy = 'ascending',
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
